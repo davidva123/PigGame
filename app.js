2bbf53c2a1537f3c5da9b0247ca4eab7ff3ac1b4
unknown <13765965+davidva123@users.noreply.github.com>
2020-10-19 14:19:56 -0400

    still having a issue with styles / want to resolve that first

--- app.js
+++ app.js
@@ -44,18 +44,57 @@ roundScore += dice;
 document.getElementById("current-" + activePlayer).textContent = roundScore;
   }
   else {
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-roundScore = 0;
-// document.getElementById("current-" + activePlayer).textContent = roundScore;
-document.getElementById("current-0").textContent = "0";
-document.getElementById("current-1").textContent = "0";
-
-document.querySelector('player-0-panel').classList.toggle('active');
-document.querySelector('player-1-panel').classList.toggle('active');
-diceDom.style.display = 'none';
+   nextPlayer();
   }
 });
 
 
 
-document.querySelector('.btn-hold')
+
+
+
+
+document.querySelector('.btn-hold').addEventListener('click',function(){
+
+// Add Current Score to global Score 
+scores[activePlayer] += roundScore;
+//Update UI
+
+document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+//Check if player wonr the game
+
+if (scores[activePlayer] >= 10)
+{
+  document.querySelector('#name-'+activePlayer).textContent = 'Winner!'
+  //game won
+document.querySelector('.dice').style.display = 'none';
+document.querySelector('.player-'+activePlayer + '-panel').classList.add('winner');
+document.querySelector('.player-'+activePlayer + '-panel').classList.remove('winner');
+
+}
+else {
+  // go to next player 
+nextPlayer();
+
+}
+
+
+
+  
+})
+
+function nextPlayer () {
+
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+roundScore = 0;
+// document.getElementById("current-" + activePlayer).textContent = roundScore;
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+document.querySelector('.player-0-panel').classList.toggle('active');
+document.querySelector('.player-1-panel').classList.toggle('active');
+diceDom.style.display = 'none';
+
+
+}
